6ed0b2c45f29d09756833460c1a24f6eb749ed20
German Leotta <german.leotta@talosdigital.com>
2021-09-28 13:39:09 +0200

    First Spring boot commit

---
+++ src/main/java/com/germanleotta/superheroes/SuperheroesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.germanleotta.superheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/germanleotta/superheroes/SuperheroesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.germanleotta.superheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
