cf02616946b28f13fec23804adc69c3b67797887
German Leotta <german.leotta@talosdigital.com>
2021-10-01 00:37:33 +0200

    first application working service /api/superheroes, unit and integration tests too

--- src/main/java/com/germanleotta/superheroes/SuperheroesDemoApplication.java
+++ src/main/java/com/germanleotta/superheroes/SuperheroesDemoApplication.java
@@ -1,10 +1,18 @@
 package com.germanleotta.superheroes;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SuperheroesDemoApplication {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesDemoApplication.class, args);
